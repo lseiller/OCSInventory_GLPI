821f7c64191722307453b1f433950ffbd4b36c93
Lowan-S <imomtmn82@relay.firefox.com>
2021-06-03 21:42:16 +0200

    Update install.sh #5
    -Correct another miss the last one for now

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ os_check(){
     fi
 
     if [ "$lsb_dist" = "debian" ]; then
-        if [ "$dist_version" != "10" ] || [ "$dist_version" != "9" ]; then
+        if [ "$dist_version" != "10" ] && [ "$dist_version" != "9" ]; then
             warn "Votre système n'est pas compatible"
             output ""
             output "OS Supporté :"
@@ -75,7 +75,7 @@ os_check(){
             exit 2
         fi
     elif [ "$lsb_dist" = "ubuntu" ]; then
-        if [ "$dist_version" != "20.10" ] || [ "$dist_version" != "20.04" ]; then
+        if [ "$dist_version" != "20.10" ] && [ "$dist_version" != "20.04" ]; then
             warn "Votre système n'est pas compatible"
             output ""
             output "OS Supporté :"
