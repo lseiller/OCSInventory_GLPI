5b4d9707a9b62e1dcce9acdf09e2ff4a1d0acbc3
Lowan-S <imomtmn82@relay.firefox.com>
2021-06-06 15:04:08 +0200

    Update on install.sh
    +Now support GLPI Install
    +MySQL setup is now independent
    +Possibility to select a specific version of OCSInventory or GLPI
    -Minor change on output
    -Minor change when installing package
    -Soon support for CentOS & Fedora
    -First beta release

--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ notsupported(){
         output "Votre système n'est pas compatible"
         output ""
         output "OS Supporté :"
-        output "Debian 10"
-        output "Debian 9"
-        output "Ubuntu 20.10"
-        output "Ubuntu 20.04"
-        output "Ubuntu 19.10"
+        output "Debian 10, 9"
+        output "Ubuntu 20.10, 20.04, 19.10, 18.04"
+        #output "CentOS Linux 8, 7"
+        #output "CentOS Stream 8"
+        #output "Fedora 33"
         exit 2
 }
 
@@ -54,8 +54,8 @@ preinstall(){
         apt update --fix-missing && apt upgrade -y
         apt-get -y install software-properties-common virt-what wget sudo
     elif [ "$lsb_dist" = "ubuntu" ]; then
-        if [ "$dist_version" = "18.04" ]; then
-            warn "Vous êtes sur Ubuntu 18.04, cette version n'est pas recommandé.\nVoulez-vous quand même procéder ?\n[1]Oui.\n[2]Non."
+        if [ "$dist_version" = "18.04" ] || [ "$dist_version" = "19.10" ]; then
+            warn "Vous êtes sur Ubuntu ${dist_version}, cette version n'est pas recommandé.\nVoulez-vous quand même procéder ?\n[1]Oui.\n[2]Non."
             read choix
             case $choix in
                 1 ) output "On continue !"
@@ -112,6 +112,10 @@ os_check(){
         if [ "$dist_version" != "20.10" ] && [ "$dist_version" != "20.04" ] && [ "$dist_version" != "19.10" ] && [ "$dist_version" != "18.04" ]; then
             notsupported
         fi
+    elif [ "$lsb_dist" = "centos" ]; then
+        if [ "$dist_version" != "7" ] && [ "$dist_version" != "8" ]; then
+            notsupported
+        fi
     else
         notsupported
     fi
@@ -122,6 +126,9 @@ option_install(){
     output "[1] Installer OCSInventory v${OCSVERSION}."
     output "[2] Installer GLPI v${GLPIVERSION}."
     output "[3] Installer GLPI v${GLPIVERSION} et OCSInventory v${OCSVERSION}."
+    output "[4] Changer la version d'OCS"
+    output "[5] Changer la version de GLPI"
+    output "[6] Reset Password root of MySQL"
     read choix
     case $choix in
         1 ) optioninstall=1
@@ -133,6 +140,18 @@ option_install(){
         3 ) optioninstall=3
             output "Vous avez choisie l'installation de GLPI et OCS."
             ;;
+        4 ) output "Indiquez la version d'OCS que vous voulez installer :"
+            read version
+            OCSVERSION=$version
+            option_install
+            ;;
+        5 ) output "Indiquez la version de GLPI que vous voulez installer :"
+            read version
+            GLPIVERSION=$version
+            option_install
+            ;;
+        6 ) curl -sSL https://raw.githubusercontent.com/tommytran732/MariaDB-Root-Password-Reset/master/mariadb-104.sh | sudo bash
+            ;;
         * ) info "Vous n'avez pas choisie d'option."
             option_install
             ;;
@@ -143,12 +162,9 @@ repositories_setup(){
     output "\nConfiguration des repo..."
     sleep 1
     if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
-        apt-get -y install curl
-        apt-get -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+        apt -y install curl
+        apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
         curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
-        if [ "$lsb_dist" = "ubuntu" ]; then
-            LC_ALL=C.UTF-8
-        fi
         apt -y update
         apt -y upgrade
         apt -y autoremove
@@ -161,7 +177,7 @@ common_dependencies(){
     sleep 1
     if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
         apt -y install apache2 libapache2-mod-perl2
-        apt -y install php php-zip php-gd php-curl php-mbstring php-soap php-xml php-mysql libapache2-mod-php php-json php-intl php-cli php-ldap php-xmlrpc php-apcu
+        apt -y install php php-curl php-gd php-json php-mbstring php-mysql php-xml php-intl php-cli php-ldap php-apcu php-xmlrpc
         apt install -y make sudo tar unzip build-essential
         apt -y install mariadb-server mariadb-client mariadb-common
     fi
@@ -169,16 +185,32 @@ common_dependencies(){
     output "\nActivation des services..."
     sleep .5
     if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
+        systemctl enable apache2
         systemctl start apache2
-        systemctl enable mariadb
-        systemctl start mariadb
     fi
+    systemctl enable mariadb
+    systemctl start mariadb
+}
+
+msyql_setup(){
+    output "\nSetup de MySQL..."
+    rootpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+    Q0="DROP DATABASE IF EXISTS test;"
+    Q1="SET old_passwords=0;"
+    Q2="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$rootpassword');"
+    Q3="DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    Q4="DELETE FROM mysql.user WHERE User='';"
+    Q5="DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+    Q6="FLUSH PRIVILEGES;"
+    SQL="${Q0}${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
+    sudo mysql -u root -e "$SQL"
 }
 
 ocs_dependencies(){
-    output "\nInstallation de perl"
+    output "\nInstallation des dependences d'OCS"
     sleep .5
     if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
+        apt install -y php-zip php-soap libapache2-mod-php
         apt install -y perl6 libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libnet-ip-perl libsoap-lite-perl libarchive-zip-perl
         if [ "$dist_version" = "9" ] || [ "$dist_version" = "18.04" ]; then
             cpan XML::Simple Compress::Zlib DBI DBD::mysql Apache::DBI Net::IP Mojolicious::Lite Plack::Handler Archive::Zip YAML XML::Entities Switch
@@ -189,11 +221,21 @@ ocs_dependencies(){
 
 }
 
+glpi_dependencies(){
+    output "\nInstallation des dependences de GLPI"
+    sleep .5
+    if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
+        apt install -y php-fileinfo php-simplexml php-cas php-bz2
+    fi
+
+}
+
 ocs_install(){
     if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
         mkdir /opt/ocs
         wget https://github.com/OCSInventory-NG/OCSInventory-ocsreports/releases/download/${OCSVERSION}/OCSNG_UNIX_SERVER-${OCSVERSION}.tar.gz -P /opt/ocs
         tar -xf /opt/ocs/OCSNG_UNIX_SERVER-${OCSVERSION}.tar.gz
+        rm /opt/ocs/OCSNG_UNIX_SERVER-${OCSVERSION}.tar.gz
         cp ./OCSNG_UNIX_SERVER-${OCSVERSION}/ /opt/ocs -r && rm ./OCSNG_UNIX_SERVER-${OCSVERSION}/ -R
         chmod +x /opt/ocs/OCSNG_UNIX_SERVER-${OCSVERSION}/setup.sh
         warn "\nDémarrage de l'installation OCS, vous allez indiquer les configurations d'ocs lors des étapes suivantes.${NC}"
@@ -203,45 +245,96 @@ ocs_install(){
     fi
 }
 
+glpi_install(){
+    if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
+        mkdir /opt/glpi
+        wget https://github.com/glpi-project/glpi/releases/download/${GLPIVERSION}/glpi-${GLPIVERSION}.tgz -P /opt/glpi
+        tar -xf /opt/glpi/glpi-${GLPIVERSION}.tgz
+        rm /opt/glpi/glpi-${GLPIVERSION}.tgz
+        cp ./glpi/ /opt/ -r && rm ./glpi/ -R
+        chmod 777 /opt/glpi/files/ -R
+        chmod 777 /opt/glpi/config/ -R
+        chmod 777 /opt/glpi/marketplace/ -R
+    fi
+}
+
 ocs_webconfig(){
     if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
         cp /etc/apache2/conf-available/ocsinventory-reports.conf /etc/apache2/sites-enabled/
-        chmod 755 /usr/share/ocsinventory-reports/ocsreports/logs/
+        chmod 777 /var/lib/ocsinventory-reports/
+        chmod 775 /usr/share/ocsinventory-reports/ocsreports/
         systemctl reload apache2
+        cd /usr/share/ocsinventory-reports/ocsreports/files/
+        mysql -f -hlocalhost -uroot -p$rootpassword ocsweb < ocsbase.sql >log.log
+    fi
+}
+
+glpi_webconfig(){
+    if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
+        glpiconf=/etc/apache2/conf-available/glpi.conf
+    fi
+    cat << EOF > $glpiconf
+Alias /glpi /opt/glpi
+
+    <Directory /opt/glpi>
+     DirectoryIndex index.php
+     Options FollowSymLinks
+     AllowOverride Limit Options FileInfo
+     Require all granted
+    </Directory>
+EOF
+    if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
+        cp /etc/apache2/conf-available/glpi.conf /etc/apache2/sites-enabled/
+        service apache2 restart
     fi
 }
 
 ocs_mysql(){
-    output "\nCréation du mot de passe pour la base de donnée..."
+    output "\nCréation de la base de donnée OCS..."
     ocs_password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-    rootpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-    Q0="DROP DATABASE IF EXISTS test;"
-    Q1="CREATE DATABASE IF NOT EXISTS ocsweb;"
-    Q2="SET old_passwords=0;"
-    Q3="GRANT ALL ON ocsweb.* TO 'ocs'@'127.0.0.1' IDENTIFIED BY '$ocs_password';"
-    Q4="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$rootpassword');"
-    Q5="DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-    Q6="DELETE FROM mysql.user WHERE User='';"
-    Q7="DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
-    Q8="FLUSH PRIVILEGES;"
-    SQL="${Q0}${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}${Q8}"
-    sudo mysql -u root -e "$SQL"
+    Q0="CREATE DATABASE IF NOT EXISTS ocsweb;"
+    Q1="GRANT ALL ON ocsweb.* TO 'ocs'@'127.0.0.1' IDENTIFIED BY '$ocs_password';"
+    Q2="FLUSH PRIVILEGES;"
+    SQL="${Q0}${Q1}${Q2}"
+    sudo mysql -u root -e "$SQL" -p$rootpassword
+}
+
+glpi_mysql(){
+    output "\nCréation de la base de donnée GLPI..."
+    glpi_password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+    Q0="CREATE DATABASE IF NOT EXISTS glpi;"
+    Q1="GRANT ALL ON glpi.* TO 'glpi'@'127.0.0.1' IDENTIFIED BY '$glpi_password';"
+    Q2="FLUSH PRIVILEGES;"
+    SQL="${Q0}${Q1}${Q2}"
+    sudo mysql -u root -e "$SQL" -p$rootpassword
 }
 
 ocs_setup(){
     ocs_dependencies
     ocs_install
-    ocs_webconfig
     ocs_mysql
+    ocs_webconfig
+}
+
+glpi_setup(){
+    glpi_dependencies
+    glpi_install
+    glpi_webconfig
+    glpi_mysql
+}
+
+broadcast_sql(){
+    output "###############################################################"
+    output "Information sur MariaDB"
+    output ""
+    output "Votre mot de passe root MySQL est : $rootpassword"
+    output ""
+    output "###############################################################"
 }
 
 broadcast_ocs(){
     output "###############################################################"
-    output "Information sur la base de donnée MariaDB"
-    output ""
-    output "Votre mot de passe root de MySQL est : $rootpassword"
-    output ""
-    output "Pour connecter OCS à la base de donnée vous aurez besoin de ces informations :"
+    output "Base de donnée OCS :"
     output "Host: 127.0.0.1 || localhost"
     output "Port: 3306"
     output "User: ocs"
@@ -251,9 +344,27 @@ broadcast_ocs(){
     output "Interface web d'OCS :"
     ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
     output "http://${ip4}/ocsreports"
-    output "Par défaut les identifiants :"
+    output "Par défaut les identifiants sont :"
     output "User: admin"
     output "Password: admin"
+    output "###############################################################"
+}
+
+broadcast_glpi(){
+    output "###############################################################"
+    output "Pour connecter GLPI à la base de donnée vous aurez besoin de ces informations :"
+    output "Host: 127.0.0.1 || localhost"
+    output "Port: 3306"
+    output "User: glpi"
+    output "Nom de la BDD: glpi"
+    output "Password: $glpi_password"
+    output ""
+    output "Interface web de GLPI :"
+    ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+    output "http://${ip4}/glpi"
+    output "Par défaut les identifiants sont :"
+    output "User: glpi"
+    output "Password: glpi"
     output "###############################################################"
 }
 
@@ -262,14 +373,28 @@ option_install
 case $optioninstall in
     1 ) repositories_setup
         common_dependencies
+        msyql_setup
         ocs_setup
+        broadcast_sql
         broadcast_ocs
         ;;
-    2 ) output "pour l'instant support uniquement ocs"
-        #repositories_setup
+    2 ) repositories_setup
+        common_dependencies
+        msyql_setup
+        glpi_setup
+        broadcast_sql
+        broadcast_glpi
         ;;
-    3 ) output "pour l'instant support uniquement ocs"
-        #repositories_setup
+    3 ) repositories_setup
+        common_dependencies
+        msyql_setup
+        ocs_setup
+        glpi_setup
+        broadcast_sql
+        sleep 5
+        broadcast_glpi
+        sleep 10
+        broadcast_ocs
         ;;
 esac
 output "Fin du script d'installation."
